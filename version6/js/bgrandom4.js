04fcf4803614e2d523af1d423a8d06dda9d4905f
flottant <53401195+flottant@users.noreply.github.com>
2020-08-17 17:59:04 +0800

    ghjkg

---
+++ version6/js/bgrandom4.js
@@ -0,0 +1,18 @@
+var xiaokang = new xkTool();
+// 设置随机背景的图片
+xiaokang.imgList = [
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/1.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/2.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/3.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/4.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/5.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/7.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/8.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/9.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/10.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/11.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/13.webp",
+    "https://s-gz-1643-blog.oss.dogecdn.com/imgcdn/14.webp",
+];
+// 调用随机背景
+xiaokang.randomBg();
